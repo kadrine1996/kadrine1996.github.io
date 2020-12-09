b4cddcf225d93a21ce75bc9fcf46d64ff363d8a5
Kadrine Lee-Taylor <71283296+kadrine1996@users.noreply.github.com>
2020-12-08 21:08:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -24,14 +24,6 @@ window.picturesData = [
     description: 'Taken in Objibway park, a provincial nature reserve in Windsor, Ontario, Canada.',
     price: 15
   },
- {
-    name: 'Under the Bridge',
-    category: 'Water Series',
-    imageUrl: 'images/item-22.JPG',
-    description:
-      'Taken in in Windsor, Ontario, Canada. Captured is the essence of Windsor, being a border city there is constant transport to and from Detroit everyday. There is an approximate of 10,000 trucks and hundreds of ships transporting cargo each day.',
-    price: 30
-  },
 
   {
     name: 'Lost Dreams',
